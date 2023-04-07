9a5a7e4974de7d01a4d384f46efda61339906032
Mateus <mateuslopes1717@gmail.com>
2023-04-07 13:56:54 -0300

    minor bugs fixed

--- src/Pages/SobreArraial/index.jsx
+++ src/Pages/SobreArraial/index.jsx
@@ -10,7 +10,7 @@ import {
 } from "./style";
 import { FaAngleLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import image from "../../assets/images/sobreArraialDesc.png";
+import image from "../../assets/images/SobreArraialDesc.png";
 
 export const SobreArraial = () => {
   const navigate = useNavigate();
